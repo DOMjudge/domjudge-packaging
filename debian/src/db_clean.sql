ff7aad869e4f856e4bac907c5c8b05edab31819f
Thijs Kinkhorst <thijs@kinkhorst.com>
2012-05-20 23:45:16 +0200

    Do not ship included copy of geshi in .deb but build depend on it.
    Remove auxiliary user creation.

--- debian/src/db_clean.sql
+++ debian/src/db_clean.sql
@@ -3,21 +3,9 @@
 
 -- In MySQL < 5.0.2, 'DROP USER' only removes the user, not its privileges:
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM
-	'domjudge_team'@'localhost',
-	'domjudge_team'@'%',
-	'domjudge_public'@'localhost',
-	'domjudge_public'@'%',
-	'domjudge_plugin'@'localhost',
-	'domjudge_plugin'@'%',
 	'domjudge_jury'@'localhost',
 	'domjudge_jury'@'%';
 DROP USER 
-	'domjudge_team'@'localhost',
-	'domjudge_team'@'%',
-	'domjudge_public'@'localhost',
-	'domjudge_public'@'%',
-	'domjudge_plugin'@'localhost',
-	'domjudge_plugin'@'%',
 	'domjudge_jury'@'localhost',
 	'domjudge_jury'@'%';
 
