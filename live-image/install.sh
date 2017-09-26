906a47c2daf2e1cdda8ab43d60201e94f962649e
Jaap Eldering <jaap@jaapeldering.nl>
2017-09-26 23:29:51 +0200

    Add mlocate and PHP zip.

--- live-image/install.sh
+++ live-image/install.sh
@@ -124,11 +124,11 @@ EOF
 
 apt-get install -q -y \
 	apt-transport-https \
-	openssh-server mysql-server apache2 sudo \
+	openssh-server mysql-server apache2 sudo php-zip \
 	gcc g++ default-jdk default-jre-headless fp-compiler ghc \
 	python-minimal python3-minimal gnat gfortran lua5.3 mono-mcs \
 	ntp phpmyadmin debootstrap cgroup-bin libcgroup1 \
-	enscript lpr zip unzip
+	enscript lpr zip unzip mlocate
 
 # Use DOMjudge debian packages if present under /tmp:
 if [ -f /tmp/domjudge-domserver_*.deb ]; then
