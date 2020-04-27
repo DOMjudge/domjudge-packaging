99f7a32e460b5a7a10ec799a4c7d970e26ffd001
Thijs Kinkhorst <thijs@kinkhorst.com>
2020-04-27 22:10:52 +0200

    More changes/simplifications for DJ >= 7.3

--- debian/src/debian-database.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if (!include($credfile)) {
-		user_error("Cannot read database credentials file " . $credfile,
-			E_USER_ERROR);
-	}
-
-	global $DB;
-
-	$DB = new db ($dbname, $dbserver, $dbuser, $dbpass, null, DJ_MYSQL_CONNECT_FLAGS);
