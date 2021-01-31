1511aa43b906fc6fbcdc61159c97ccfa2b2e0a42
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-01-31 18:39:38 +0100

    Build gitlabci/maintainer image on main branch
    On the PR stage we only test if the build succeeds.
    When merged to main branch this will push to the GitLab registry.
    
    As Bash is not available in the docker(dind) we fallback to sh.

--- docker-gitlabci/build.sh
+++ docker-gitlabci/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+if [[ ! -z ${CI} ]]
+then
+        set -euxo pipefail
+        export PS4='(${0}:${LINENO}): - [$?] $ '
+fi
+
 VERSION=$1
 if [[ -z ${VERSION} ]]
 then
@@ -7,14 +13,25 @@ then
 	echo "	For example: $0 1.0"
 	exit 1
 fi
+if [[ -z $2 ]]
+then
+	REGISTRY="domjudge/gitlabci"
+else
+	REGISTRY=$2
+fi
 
 echo "[..] Building Docker image for Gitlab CI..."
 cp -r ../docker-contributor/php-config ./
-docker build -t domjudge/gitlabci:${VERSION} . 
+docker build -t ${REGISTRY}:${VERSION} . 
 rm -r php-config
 echo "[ok] Done building Docker image for Gitlab CI"
 
-echo "All done. Image domjudge/gitlabci:${VERSION} created"
-echo "If you are a DOMjudge maintainer with access to the domjudge organization on Docker Hub, you can now run the following command to push them to Docker Hub:"
-echo "$ docker push domjudge/gitlabci:${VERSION}"
-echo "You probably will want to update .gitlab-ci.yml to point to this new image in the DOMjudge/domjudge repository."
+if [[ -z ${CI} ]]
+then
+        echo "All done. Image ${REGISTRY}:${VERSION} created"
+        echo "If you are a DOMjudge maintainer with access to the domjudge organization on Docker Hub, you can now run the following command to push them to Docker Hub:"
+        echo "$ docker push ${REGISTRY}:${VERSION}"
+        echo "You probably will want to update .gitlab-ci.yml to point to this new image in the DOMjudge/domjudge repository."
+else
+        echo "When this is not a PR, we will try to upload this to the GitLab registry."
+fi
