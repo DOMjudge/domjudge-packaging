6022ceece575f4b4a4ba73630792c108683c7c65
Dup4 <lyuzhi.pan@gmail.com>
2021-11-16 17:32:57 +0800

    feat: manual install deps

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -3,21 +3,21 @@
 # Add packages with -i "<apt package name>" here
 /opt/domjudge/judgehost/bin/dj_make_chroot
 
-# CHROOTDIR=/chroot/domjudge
-# INSTALLDEBS="gcc g++ make default-jdk-headless default-jre-headless pypy pypy3 python3 locales"
+CHROOTDIR=/chroot/domjudge
+INSTALLDEBS="gcc g++ make default-jdk-headless default-jre-headless pypy pypy3 python3 locales"
 
-# mount -t proc proc "$CHROOTDIR"/proc
-# mount -t sysfs sysfs "$CHROOTDIR"/sys
-# mount --bind /dev/pts "$CHROOTDIR"/dev/pts
+mount -t proc proc "$CHROOTDIR"/proc
+mount -t sysfs sysfs "$CHROOTDIR"/sys
+mount --bind /dev/pts "$CHROOTDIR"/dev/pts
 
-# chroot "$CHROOTDIR" /bin/sh -c "apt-get update && apt-get dist-upgrade"
-# chroot "$CHROOTDIR" /bin/sh -c "apt-get clean"
-# chroot "$CHROOTDIR" /bin/sh -c "apt-get install $INSTALLDEBS"
-# chroot "$CHROOTDIR" /bin/sh -c "apt-get clean"
+chroot "$CHROOTDIR" /bin/sh -c "apt-get update && apt-get dist-upgrade"
+chroot "$CHROOTDIR" /bin/sh -c "apt-get clean"
+chroot "$CHROOTDIR" /bin/sh -c "apt-get install $INSTALLDEBS"
+chroot "$CHROOTDIR" /bin/sh -c "apt-get clean"
 
-# umount "$CHROOTDIR/dev/pts"
-# umount "$CHROOTDIR/sys"
-# umount "$CHROOTDIR/proc"
+umount "$CHROOTDIR/dev/pts"
+umount "$CHROOTDIR/sys"
+umount "$CHROOTDIR/proc"
 
 cd /
 echo "[..] Compressing chroot"
