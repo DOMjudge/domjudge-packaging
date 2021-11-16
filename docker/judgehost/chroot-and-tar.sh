5b186d5540c9f5bd2f992ac4d6afa08e6e6ee6bf
Dup4 <lyuzhi.pan@gmail.com>
2021-11-16 11:40:46 +0800

    feat: add package deps in chroot

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add packages with -i "<apt package name>" here
-/opt/domjudge/judgehost/bin/dj_make_chroot
+/opt/domjudge/judgehost/bin/dj_make_chroot -i "gcc g++ make default-jdk-headless default-jre-headless pypy pypy3 python3 locales"
 
 cd /
 echo "[..] Compressing chroot"
