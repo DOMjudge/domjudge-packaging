d4a2543f57009cbd3b37adec92a1e59656949f11
Dup4 <lyuzhi.pan@gmail.com>
2021-11-25 22:48:19 +0800

    fix: set the timezone in php.ini when the container start

--- docker/domserver/scripts/start.d/10-timezone.sh
+++ docker/domserver/scripts/start.d/10-timezone.sh
@@ -4,3 +4,9 @@ ln -snf /usr/share/zoneinfo/${CONTAINER_TIMEZONE} /etc/localtime
 echo ${CONTAINER_TIMEZONE} > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 echo "[ok] Container timezone set to: ${CONTAINER_TIMEZONE}"; echo
+
+# Configure php
+php_folder=$(echo "/etc/php/7."?"/")
+
+sed -ri -e "s#^;date\.timezone.*#date.timezone = ${CONTAINER_TIMEZONE}#" \
+	"$php_folder/fpm/php.ini"
