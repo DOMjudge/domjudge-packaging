5e325e818a65614945fe9c0aa47cfebe33610c2c
Dup4 <lyuzhi.pan@gmail.com>
2021-11-29 01:16:56 +0800

    fix: SC2046

--- docker/domserver/scripts/start.sh
+++ docker/domserver/scripts/start.sh
@@ -13,10 +13,10 @@ for i in /scripts/start.d/*
 do
 	if [ -x "$i" ]
 	then
-		echo "[..] Running start script" $(basename "$i")
+		echo "[..] Running start script" "$(basename "$i")"
 		if ! "$i"
 		then
-			echo "[!!] Start script" $(basename "$i") "failed"
+			echo "[!!] Start script" "$(basename "$i")" "failed"
 			exit 1
 		fi
 	fi
