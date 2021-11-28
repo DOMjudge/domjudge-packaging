321368413b1771d5ed8d584e3733fa9384776e78
Dup4 <lyuzhi.pan@gmail.com>
2021-11-28 22:58:57 +0800

    fix: typo

--- docker/domserver/build.sh
+++ docker/domserver/build.sh
@@ -4,7 +4,7 @@ cd /domjudge-src/domjudge*
 chown -R domjudge: .
 # If we used a local source tarball, it might not have been built yet
 sudo -u domjudge make dist
-sudo -u domjudge ./configure --with-baseurl=http://localhost/
+sudo -u domjudge ./configure -with-baseurl=http://localhost/
 
 # Passwords should not be included in the built image. We create empty files here to prevent passwords from being generated.
 sudo -u domjudge touch etc/dbpasswords.secret etc/restapi.secret etc/symfony_app.secret etc/initial_admin_password.secret
