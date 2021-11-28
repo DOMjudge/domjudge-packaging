481591cba29fc1cecebb9482b8efa3ec6dd9726f
Dup4 <lyuzhi.pan@gmail.com>
2021-11-28 14:09:44 +0800

    fix: add domjudge user

--- docker/judgehost/build.sh
+++ docker/judgehost/build.sh
@@ -6,7 +6,7 @@ chown -R domjudge: .
 sudo -u domjudge make configure
 sudo -u domjudge ./configure -with-baseurl=http://localhost/
 
-echo "default	http://localhost/api	dummy	dummy" | sudo tee etc/restapi.secret > /dev/null
+echo "default	http://localhost/api	dummy	dummy" | sudo -u domjudge tee etc/restapi.secret > /dev/null
 
 sudo -u domjudge make judgehost
 make install-judgehost
