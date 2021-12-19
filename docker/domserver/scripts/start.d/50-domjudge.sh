1eb645c0b86d33e683980b2eca6ced96b7d651ee
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-12-19 20:03:40 +0100

    Replace legacy syntax as advised by shellcheck
    No functional changes intended

--- docker/domserver/scripts/start.d/50-domjudge.sh
+++ docker/domserver/scripts/start.d/50-domjudge.sh
@@ -198,7 +198,7 @@ do
 	if ! mysqlshow -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h"${MYSQL_HOST}" "${MYSQL_DATABASE}" > /dev/null 2>&1
 	then
 		echo "MySQL database ${MYSQL_DATABASE} not yet found on host ${MYSQL_HOST};"
-		let "DB_UP--"
+		(( DB_UP-- ))
 		sleep 10s
 	else
 		DB_UP=0
