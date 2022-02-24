6112cc8cebccdda9c45252848337661025fce203
Dup4 <lyuzhi.pan@gmail.com>
2022-02-24 13:18:07 +0800

    feat: support git clone

--- docker/download.sh
+++ docker/download.sh
@@ -1,21 +1,36 @@
 #! /bin/bash
 
 TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+FILE_NAME="domjudge"
+FILE="${TOP_DIR}/${FILE_NAME}.tar.gz"
+DIR="${TOP_DIR}/${FILE_NAME}"
 
 VERSION="$1"
 REPO="$2"
 
-if [[ X"${VERSION}" = X"latest" ]]; then
-	URL=https://codeload.github.com/${REPO}/tar.gz/refs/heads/main
+if command -v git >/dev/null 2>&1; then
+	GIT_COMMAND="git clone https://github.com/DOMjudge/domjudge.git --depth=1"
+	if [[ X"${VERSION}" != X"latest" ]]; then
+		GIT_COMMAND="${GIT_COMMAND} -b ${VERSION}"
+	fi
+
+	GIT_COMMAND="${GIT_COMMAND} ${DIR}"
+
+	${GIT_COMMAND}
+
+	tar -cvzf "${FILE}" -C "${TOP_DIR}" "${FILE_NAME}"
+	rm -rf "${DIR}"
 else
-	URL=https://codeload.github.com/${REPO}/tar.gz/refs/tags/${VERSION}
-fi
+	if [[ X"${VERSION}" = X"latest" ]]; then
+		URL=https://codeload.github.com/${REPO}/tar.gz/refs/heads/main
+	else
+		URL=https://codeload.github.com/${REPO}/tar.gz/refs/tags/${VERSION}
+	fi
 
-FILE="${TOP_DIR}"/domjudge.tar.gz
+	echo "[..] Downloading DOMjudge version ${VERSION}..."
 
-echo "[..] Downloading DOMjudge version ${VERSION}..."
-
-if ! wget --quiet "${URL}" -O "${FILE}"; then
-	echo "[!!] DOMjudge version ${VERSION} file not found on https://codeload.github.com/DOMjudge/domjudge"
-	exit 1
+	if ! wget --quiet "${URL}" -O "${FILE}"; then
+		echo "[!!] DOMjudge version ${VERSION} file not found on https://codeload.github.com/DOMjudge/domjudge"
+		exit 1
+	fi
 fi
