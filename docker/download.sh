f70b5460aaa2084fa3078edfdd8297f63ad6989d
Dup4 <lyuzhi.pan@gmail.com>
2022-05-19 10:10:28 +0800

    chore: style format

--- docker/download.sh
+++ docker/download.sh
@@ -9,36 +9,36 @@ VERSION="$1"
 REPO="$2"
 
 if [[ -z "${VERSION}" ]]; then
-	VERSION="latest"
+    VERSION="latest"
 fi
 
 if [[ -z "${REPO}" ]]; then
-	REPO="DOMjudge/domjudge"
+    REPO="DOMjudge/domjudge"
 fi
 
 if command -v git >/dev/null 2>&1; then
-	GIT_COMMAND="git clone https://github.com/${REPO}.git --depth=1"
-	if [[ X"${VERSION}" != X"latest" ]]; then
-		GIT_COMMAND="${GIT_COMMAND} -b ${VERSION}"
-	fi
+    GIT_COMMAND="git clone https://github.com/${REPO}.git --depth=1"
+    if [[ X"${VERSION}" != X"latest" ]]; then
+        GIT_COMMAND="${GIT_COMMAND} -b ${VERSION}"
+    fi
 
-	GIT_COMMAND="${GIT_COMMAND} ${DIR}"
+    GIT_COMMAND="${GIT_COMMAND} ${DIR}"
 
-	${GIT_COMMAND}
+    ${GIT_COMMAND}
 
-	tar -cvzf "${FILE}" -C "${TOP_DIR}" "${FILE_NAME}"
-	rm -rf "${DIR}"
+    tar -cvzf "${FILE}" -C "${TOP_DIR}" "${FILE_NAME}"
+    rm -rf "${DIR}"
 else
-	if [[ X"${VERSION}" = X"latest" ]]; then
-		URL=https://codeload.github.com/${REPO}/tar.gz/refs/heads/main
-	else
-		URL=https://codeload.github.com/${REPO}/tar.gz/refs/tags/${VERSION}
-	fi
+    if [[ X"${VERSION}" = X"latest" ]]; then
+        URL=https://codeload.github.com/${REPO}/tar.gz/refs/heads/main
+    else
+        URL=https://codeload.github.com/${REPO}/tar.gz/refs/tags/${VERSION}
+    fi
 
-	echo "[..] Downloading DOMjudge version ${VERSION}..."
+    echo "[..] Downloading DOMjudge version ${VERSION}..."
 
-	if ! wget --quiet "${URL}" -O "${FILE}"; then
-		echo "[!!] DOMjudge version ${VERSION} file not found on https://codeload.github.com/DOMjudge/domjudge"
-		exit 1
-	fi
+    if ! wget --quiet "${URL}" -O "${FILE}"; then
+        echo "[!!] DOMjudge version ${VERSION} file not found on https://codeload.github.com/DOMjudge/domjudge"
+        exit 1
+    fi
 fi
