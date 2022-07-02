da8f277763e9b6df0ba4a3a1ff5364b6921c3b01
Robert van Dijk <robert@citroenvlinder17.nl>
2022-07-02 11:23:17 +0200

    Update icpc repo

--- docker/judgehost/add_repositories.sh
+++ docker/judgehost/add_repositories.sh
@@ -1,8 +1,8 @@
 apt-get update
 apt-get install software-properties-common gnupg
 
-echo "deb [trusted=yes] https://pc2.ecs.baylor.edu/apt focal main" >> /etc/apt/sources.list
-echo 'Acquire::https::pc2.ecs.baylor.edu::Verify-Peer "false";
-Acquire::https::pc2.ecs.baylor.edu::Verify-Host "false";' >> /etc/apt/apt.conf.d/80trust-baylor-mirror
+echo "deb [trusted=yes] https://sysopspackages.icpc.global/apt focal main" >> /etc/apt/sources.list
+echo 'Acquire::https::sysopspackages.icpc.global::Verify-Peer "false";
+Acquire::https::sysopspackages.icpc.global::Verify-Host "false";' >> /etc/apt/apt.conf.d/80trust-baylor-mirror
 
 apt-get update
