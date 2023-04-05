fedd37824f5b8ab657cd0703906f8483843b866e
Dup4 <lyuzhi.pan@gmail.com>
2023-04-05 22:37:24 +0800

    feat: use Ubuntu jammy to make chroot
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage: https://github.com/DOMjudge/domjudge/blob/main/misc-tools/dj_make_chroot.in#L58-L87
-/opt/domjudge/judgehost/bin/dj_make_chroot
+/opt/domjudge/judgehost/bin/dj_make_chroot -D Ubuntu -R jammy
 
 cd /
 echo "[..] Compressing chroot"
