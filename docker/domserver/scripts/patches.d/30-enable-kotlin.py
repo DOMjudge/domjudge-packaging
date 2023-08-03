45250ef27c012c8473004748c103519570e55a2f
Robert van Dijk <robert@citroenvlinder17.nl>
2023-08-03 20:39:14 +0200

    Update 30-enable-kotlin.py
    Make it compatible with future domjusge versions

--- docker/domserver/scripts/patches.d/30-enable-kotlin.py
+++ docker/domserver/scripts/patches.d/30-enable-kotlin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import pathlib
+import re
 
 dj_source = next(pathlib.Path("/domjudge-src").glob("domjudge*"))
 language_fixture_php = dj_source / "webapp/src/DataFixtures/DefaultData/LanguageFixture.php"
@@ -9,9 +10,14 @@ with open(language_fixture_php, "r") as f:
      lines = f.readlines()
 
 for i in range(len(lines)):
-    if lines[i] == "            ['kt',     'kotlin',     'Kotlin',      ['kt'],                      true,  'Main class', false,  true,   1,     'kt'],\n":
-        lines[i] = "            ['kt',     'kotlin',     'Kotlin',      ['kt'],                      true,  'Main class', true,   true,   1,     'kt'],\n"
-        break
+    elements = lines[i].split(',')
+
+    if len(elements) >= 10 and elements[6].strip().lower() == 'false':
+        if 'kt' in elements[0]:
+            elements[6] = 'true'
+
+            lines[i] = ','.join(elements)
+            break
 else:
     print(f"Couldn't find kotlin line in {language_fixture_php}")
     exit(1)
