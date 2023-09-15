bc956ade0e8d8d2760357f122073d69738f6b131
Maarten Weyns <maarten.weyns@mawey.be>
2023-09-15 17:43:28 +0200

    Update judgehost and apt repo to jammy

--- docker/judgehost/add_repositories.sh
+++ docker/judgehost/add_repositories.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install software-properties-common gnupg
 
-echo "deb [trusted=yes] https://sysopspackages.icpc.global/apt focal main" >> /etc/apt/sources.list
+echo "deb [trusted=yes] https://sysopspackages.icpc.global/apt/jammy jammy main" >> /etc/apt/sources.list
 echo 'Acquire::https::sysopspackages.icpc.global::Verify-Peer "false";
 Acquire::https::sysopspackages.icpc.global::Verify-Host "false";' >> /etc/apt/apt.conf.d/80trust-baylor-mirror
 
--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -3,7 +3,7 @@
 sed -i.bak '/# Upgrade the system, and install\/remove packages as desired/i mkdir \"\$CHROOTDIR\/scripts\/" && cp \/scripts\/add_repositories.sh \"\$CHROOTDIR\/scripts\/" && chmod -R 755 \"\$CHROOTDIR\/scripts\/" && in_chroot \"\/scripts\/add_repositories.sh\"\n' /opt/domjudge/judgehost/bin/dj_make_chroot
 
 # Usage: https://github.com/DOMjudge/domjudge/blob/main/misc-tools/dj_make_chroot.in#L58-L87
-/opt/domjudge/judgehost/bin/dj_make_chroot -i openjdk-17-jre-headless,openjdk-17-jdk-headless,pypy3,icpc-kotlinc
+/opt/domjudge/judgehost/bin/dj_make_chroot -R jammy -i openjdk-17-jre-headless,openjdk-17-jdk-headless,pypy3,icpc-kotlinc
 
 cd /
 echo "[..] Compressing chroot"
