8944e11a8ea1c38e7b7697895a9ab64d9d7848a8
Michael <14887731+vmcj@users.noreply.github.com>
2024-01-12 15:56:30 +0100

    Create action for docker build scripts
    The PRs for changes to those scripts will be stored in the registry
    of the user/organisation which forked or in our GitHub docker registry
    if this branch is under the domjudge organization. Here we always build
    against our latest version.
    
    The GitLab code had the option to not push the latest tag, for when we
    rebuild an older container, otherwise we always release against the
    overwritten value or if nothing was provided against the latest released
    tag (so which latest points to). The code for world readable files has
    been kept.
    
    Our build script is extended to now also have an option to push to
    another organization/namespace so we can push the image to the github
    container registry of the person doing the PR.
    
    As we don't do this often we explicit clean the github runner of older
    versions to make sure we always build against the latest image available
    of our dependencies and don't encounter the earlier builds if a PR is
    done more often (to fix something for example).
    
    The image can be locally tested by looking at the special tag based on
    the branchname/issue_number.

--- docker/build.sh
+++ docker/build.sh
@@ -6,14 +6,20 @@ then
 	export PS4='(${0}:${LINENO}): - [$?] $ '
 fi
 
-if [ "$#" -ne 1 ]
+if [ "$#" -eq 0 ] || [ "$#" -gt 2 ]
 then
-	echo "Usage: $0 domjudge-version"
+	echo "Usage: $0 domjudge-version <namespace>"
 	echo "	For example: $0 5.3.0"
+	echo "	or: $0 5.3.0 otherNamespace"
 	exit 1
 fi
 
 VERSION="$1"
+NAMESPACE="domjudge"
+if [ -n "${2+x}" ]
+then
+	NAMESPACE="$2"
+fi
 
 URL=https://www.domjudge.org/releases/domjudge-${VERSION}.tar.gz
 FILE=domjudge.tar.gz
@@ -29,22 +35,22 @@ fi
 echo "[ok] DOMjudge version ${VERSION} downloaded as domjudge.tar.gz"; echo
 
 echo "[..] Building Docker image for domserver..."
-./build-domjudge.sh "domjudge/domserver:${VERSION}"
+./build-domjudge.sh "${NAMESPACE}/domserver:${VERSION}"
 echo "[ok] Done building Docker image for domserver"
 
 echo "[..] Building Docker image for judgehost using intermediate build image..."
-./build-judgehost.sh "domjudge/judgehost:${VERSION}"
+./build-judgehost.sh "${NAMESPACE}/judgehost:${VERSION}"
 echo "[ok] Done building Docker image for judgehost"
 
 echo "[..] Building Docker image for judgehost chroot..."
-docker build -t "domjudge/default-judgehost-chroot:${VERSION}" -f judgehost/Dockerfile.chroot .
+docker build -t "${NAMESPACE}/default-judgehost-chroot:${VERSION}" -f judgehost/Dockerfile.chroot .
 echo "[ok] Done building Docker image for judgehost chroot"
 
-echo "All done. Image domjudge/domserver:${VERSION} and domjudge/judgehost:${VERSION} created"
+echo "All done. Image ${NAMESPACE}/domserver:${VERSION} and ${NAMESPACE}/judgehost:${VERSION} created"
 echo "If you are a DOMjudge maintainer with access to the domjudge organization on Docker Hub, you can now run the following command to push them to Docker Hub:"
-echo "$ docker push domjudge/domserver:${VERSION} && docker push domjudge/judgehost:${VERSION} && docker push domjudge/default-judgehost-chroot:${VERSION}"
+echo "$ docker push ${NAMESPACE}/domserver:${VERSION} && docker push ${NAMESPACE}/judgehost:${VERSION} && docker push $NAMESPACE}/default-judgehost-chroot:${VERSION}"
 echo "If this is the latest release, also run the following command:"
-echo "$ docker tag domjudge/domserver:${VERSION} domjudge/domserver:latest && \
-docker tag domjudge/judgehost:${VERSION} domjudge/judgehost:latest && \
-docker tag domjudge/default-judgehost-chroot:${VERSION} domjudge/default-judgehost-chroot:latest && \
-docker push domjudge/domserver:latest && docker push domjudge/judgehost:latest && docker push domjudge/default-judgehost-chroot:latest"
+echo "$ docker tag ${NAMESPACE}/domserver:${VERSION} ${NAMESPACE}/domserver:latest && \
+docker tag ${NAMESPACE}/judgehost:${VERSION} ${NAMESPACE}/judgehost:latest && \
+docker tag ${NAMESPACE}/default-judgehost-chroot:${VERSION} ${NAMESPACE}/default-judgehost-chroot:latest && \
+docker push ${NAMESPACE}/domserver:latest && docker push ${NAMESPACE}/judgehost:latest && docker push ${NAMESPACE}/default-judgehost-chroot:latest"
