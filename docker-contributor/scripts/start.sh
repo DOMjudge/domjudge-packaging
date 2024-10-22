DOMjudge/domjudge-packaging

commit 628381697202122168e7e227e36a7562a492471f

Add Apache2 configuration to docker-contributor
Adding configuration for Apache2 enables easier testing of
webserver-specific features and issues.

By default, the contributor image still uses NGINX as webserver. Add an
option to use Apache2 by default, or switch back and forth between
NGINX/Apache2 with the `switch-webserver` command.

docker-contributor/scripts/start.sh
@@ -149,6 +149,23 @@ sudo sed -i '/error_log/d' $NGINX_CONFIG_FILE
 # Use debug front controller
 sudo sed -i 's/app\.php/app_dev.php/g' $NGINX_CONFIG_FILE
 sudo sed -i 's/app\\\.php/app\\_dev.php/g' $NGINX_CONFIG_FILE
+
+# Configure Apache2
+APACHE2_CONFIG_FILE=/etc/apache2/conf-available/domjudge.conf
+sudo cp etc/apache.conf $APACHE2_CONFIG_FILE
+sudo a2enmod proxy_fcgi setenvif rewrite
+sudo cp "/etc/apache2/conf-available/php$DEFAULTPHPVERSION-fpm.conf" /etc/apache2/conf-available/php-domjudge-fpm.conf
+sudo sed -i 's/proxy:unix:.*|/proxy:unix:\/var\/run\/php-fpm-domjudge.sock|/' /etc/apache2/conf-available/php-domjudge-fpm.conf
+sudo a2enconf php-domjudge-fpm domjudge
+sudo rm /etc/apache2/sites-enabled/000-default.conf
+# Run DOMjudge in root
+sudo sed -i '/^#<VirtualHost \*>/,/^#<\/VirtualHost>/ s/#//' $APACHE2_CONFIG_FILE
+sudo sed -i 's/^Alias \/domjudge/#Alias \/domjudge/' $APACHE2_CONFIG_FILE
+# Run as user and group 'domjudge'
+sudo sed -i 's/<VirtualHost \*>/User domjudge\nGroup domjudge\n<VirtualHost \*>/' $APACHE2_CONFIG_FILE
+# Redirect logs to stdout/stderr
+sudo sed -i 's/<VirtualHost \*>/TransferLog \/dev\/stdout\nErrorLog \/dev\/stderr\n<VirtualHost \*>/' $APACHE2_CONFIG_FILE
+
 # Set up permissions (make sure the script does not stop if this fails, as this will happen on macOS / Windows)
 sudo chown domjudge: "${PROJECT_DIR}/webapp/var"
 echo "[ok] Webserver config installed"; echo
@@ -175,5 +192,15 @@ echo "[ok] Sudoers configuration added"; echo
 
 sudo sed -i "s|PROJECT_DIR|${PROJECT_DIR}|" /etc/supervisor/conf.d/judgedaemon.conf
 sudo sed -i "s|PROJECT_DIR|${PROJECT_DIR}|" /etc/supervisor/conf.d/judgedaemonextra.conf
+
+echo "[..] Configuring default webserver"
+if [ "${DEFAULTWEBSERVER}" = "apache2" ] || [ "${DEFAULTWEBSERVER}" = "nginx" ]
+then
+  sudo sed -i "s|autostart=false|autostart=true|" "/etc/supervisor/conf.d/$DEFAULTWEBSERVER.conf"
+else
+  echo "Unsupported webserver '$DEFAULTWEBSERVER'"
+  exit 1
+fi
+echo "[ok] Configured default webserver"; echo
 
 exec sudo supervisord -n -c /etc/supervisor/supervisord.conf

docker-contributor/Dockerfile
@@ -16,6 +16,7 @@ ENV DEBIAN_FRONTEND=noninteractive \
   DJ_DB_INSTALL_BARE=0 \
   PHPSUPPORTED="8.1 8.2 8.3" \
   DEFAULTPHPVERSION="8.3" \
+  DEFAULTWEBSERVER="nginx" \
   APTINSTALL="apt-get install -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold"
 
 # Install required packages and clean up afterwards to make this image layer smaller

docker-contributor/scripts/bin/apache2-access-log
@@ -1,0 +1,2 @@
+#!/bin/bash
+supervisorctl tail -f apache2

docker-contributor/scripts/bin/apache2-error-log
@@ -1,0 +1,2 @@
+#!/bin/bash
+supervisorctl tail -f apache2 stderr

docker-contributor/scripts/bin/switch-webserver
@@ -1,0 +1,14 @@
+#!/bin/bash
+WEBSERVER=$1
+if [ "${WEBSERVER}" = "nginx" ]
+then
+  sudo supervisorctl stop apache2
+  sudo supervisorctl start nginx
+elif [ "${WEBSERVER}" = "apache2" ]
+then
+  sudo supervisorctl stop nginx
+  sudo supervisorctl start apache2
+else
+  echo "Usage: $0 [apache2|nginx]"
+  exit 1
+fi

docker-contributor/supervisor/apache2.conf
@@ -1,0 +1,5 @@
+[program:apache2]
+command=pidproxy /var/run/apache2/apache2.pid /bin/bash -c "source /etc/apache2/envvars && apache2ctl -D FOREGROUND"
+numprocs=1
+autostart=false
+autorestart=true

docker-contributor/supervisor/nginx.conf
@@ -1,5 +1,5 @@
 [program:nginx]
 command=nginx -g "daemon off;"
 numprocs=1
-autostart=true
+autostart=false
 autorestart=true

commit b89bcf6d9c18aa5aa9764034fad9ce6de303cc3e

Allow deletion of default config to fail
If the container is restarted, the configuration file does not exist. Do
not fail the `rm` command if the file could not be removed to ensure
idempotency of the Apache2 configuration part.

Fixes an issue introduced in 628381697202122168e7e227e36a7562a492471f.

docker-contributor/scripts/start.sh
@@ -157,7 +157,7 @@ sudo a2enmod proxy_fcgi setenvif rewrite
 sudo cp "/etc/apache2/conf-available/php$DEFAULTPHPVERSION-fpm.conf" /etc/apache2/conf-available/php-domjudge-fpm.conf
 sudo sed -i 's/proxy:unix:.*|/proxy:unix:\/var\/run\/php-fpm-domjudge.sock|/' /etc/apache2/conf-available/php-domjudge-fpm.conf
 sudo a2enconf php-domjudge-fpm domjudge
-sudo rm /etc/apache2/sites-enabled/000-default.conf
+sudo rm -f /etc/apache2/sites-enabled/000-default.conf
 # Run DOMjudge in root
 sudo sed -i '/^#<VirtualHost \*>/,/^#<\/VirtualHost>/ s/#//' $APACHE2_CONFIG_FILE
 sudo sed -i 's/^Alias \/domjudge/#Alias \/domjudge/' $APACHE2_CONFIG_FILE