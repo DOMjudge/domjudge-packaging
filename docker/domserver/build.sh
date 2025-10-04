ec5004cec51281793cda9f81f558a5053388e7d4
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2025-10-04 13:12:11 +0200

    Assume people build a supported DOMjudge version

--- docker/domserver/build.sh
+++ docker/domserver/build.sh
@@ -4,7 +4,7 @@ cd /domjudge-src/domjudge*
 chown -R domjudge: .
 # If we used a local source tarball, it might not have been built yet
 sudo -u domjudge sh -c '. /venv/bin/activate && make dist'
-sudo -u domjudge ./configure -with-baseurl=http://localhost/
+sudo -u domjudge ./configure -with-baseurl=http://localhost/ --disable-judgehost-build
 
 # Passwords should not be included in the built image. We create empty files here to prevent passwords from being generated.
 sudo -u domjudge touch etc/dbpasswords.secret etc/restapi.secret etc/symfony_app.secret etc/initial_admin_password.secret
