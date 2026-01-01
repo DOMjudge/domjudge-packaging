1edfee4cd4583b25ee08df88626b06046bd6d440
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2026-01-01 17:40:35 +0100

    Revert from using the venv for docs building
    The current Ubuntu/Debian have the right version for sphinx again.

--- docker/domserver/build.sh
+++ docker/domserver/build.sh
@@ -3,7 +3,7 @@
 cd /domjudge-src/domjudge*
 chown -R domjudge: .
 # If we used a local source tarball, it might not have been built yet
-sudo -u domjudge sh -c '. /venv/bin/activate && make dist'
+sudo -u domjudge sh -c 'make dist'
 sudo -u domjudge ./configure -with-baseurl=http://localhost/ --disable-judgehost-build
 
 # Passwords should not be included in the built image. We create empty files here to prevent passwords from being generated.
@@ -15,8 +15,5 @@ make install-domserver
 # Remove installed password files
 rm /opt/domjudge/domserver/etc/*.secret
 
-sudo -u domjudge sh -c '. /venv/bin/activate && make docs'
-# Use Python venv to use the latest Sphinx to build DOMjudge docs.
-# shellcheck source=/dev/null
-. /venv/bin/activate
+sudo -u domjudge sh -c 'make docs'
 make install-docs
